cb31aa0bf4e75ebe64cd05630d549259297a0314
Tristan Vigar <tristanvigar@protonmail.com>
2020-03-12 12:20:08 -0600

    Added: All GET requests return the request object. All other requests return status_code

--- monday_v1.py
+++ monday_v1.py
@@ -7,7 +7,7 @@ api_url = 'https://api.monday.com'
 
 ### Users ###
 
-def get_all_users(secret, api_url, page_offset, results_per_page, result_offset, order_by_latest):
+def get_all_users(secret, api_url, page_offset=0, results_per_page=25, result_offset=0, order_by_latest=False):
     data = {
             'api_key': secret,
             'page': page_offset,
@@ -25,10 +25,10 @@ def get_user(secret, user_id):
             'id': user_id
            }
     endpoint = f'/v1/users/{user_id}.json'
-    r = requests.get(url=url+endpoint)
+    r = requests.get(url=url+endpoint, data=data)
     return r
 
-def get_specific_user_post(secret, api_url, user_id, page_offset, results_per_page, result_offset):
+def get_specific_user_post(secret, api_url, user_id, page_offset=0, results_per_page=25, result_offset=0):
     data = {
             'api_key': secret,
             'page': page_offset,
@@ -36,10 +36,10 @@ def get_specific_user_post(secret, api_url, user_id, page_offset, results_per_pa
             'offset': result_offset
            }
     endpoint = f'/v1/users/{user_id}/posts.json'
-    r = requests.
+    r = requests.get(url=api_url+endpoint, data=data)
     return r
 
-def get_specific_user_newsfeed(secret, api_url, user_id, page_offset, results_per_page, result_offset):
+def get_specific_user_newsfeed(secret, api_url, user_id, page_offset=0, results_per_page=25, result_offset=0):
     data = {
             'api_key': secret,
             'id': user_id,
@@ -53,7 +53,7 @@ def get_specific_user_newsfeed(secret, api_url, user_id, page_offset, results_pe
 
 ### Updates ###
 
-def get_all_account_updates(secret, api_url, page_offset, results_per_page, result_offset, since_date, until_date, updated_since_date, updated_until_date):
+def get_all_account_updates(secret, api_url, page_offset=0, results_per_page=25, result_offset=0, since_date='', until_date='', updated_since_date='', updated_until_date=''):
     data = {
             'api_key': secret,
             'page': page_offset,
@@ -77,7 +77,7 @@ def create_update(secret, api_url, user_id, pulse_id, update_text):
            }
     endpoint = '/v1/updates.json'
     r = requests.post(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def get_specific_post(secret, api_url, update_id):
     data = {
@@ -95,7 +95,7 @@ def delete_update(secret, api_url, update_id):
            }
     endpoint = f'/v1/updates/{update_id}.json'
     r = requests.delete(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def like_update(secret, api_url, update_id, user_id):
     data = {
@@ -105,7 +105,7 @@ def like_update(secret, api_url, update_id, user_id):
            }
     endpoint = f'/v1/updates/{update_id}/like.json'
     r = requests.post(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def unlike_update(secret, api_url, update_id, user_id):
     data = {
@@ -115,11 +115,11 @@ def unlike_update(secret, api_url, update_id, user_id):
            }
     endpoint = f'/v1/updates/{update_id}/unlike.json'
     r = requests.post(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 ### Pulses ###
 
-def get_all_account_pulses(secret, api_url, page_offset, results_per_page, result_offset, order_by_latest, since_date, until_date):
+def get_all_account_pulses(secret, api_url, page_offset=0, results_per_page=25, result_offset=0, order_by_latest=False, since_date='', until_date=''):
     data = {
             'api_key': secret,
             'page': page_offset,
@@ -150,68 +150,90 @@ def update_pulse_attributes(secret, api_url, pulse_id, pulse_name):
            }
     endpoint = f'/v1/pulses/{pulse_id}.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
-def delete_pulse(secret, api_url, pulse_id, archive_pulse):
+def delete_pulse(secret, api_url, pulse_id, archive_pulse=False):
     data = {
             'api_key': secret,
             'id': pulse_id,
+            'archive': archive_pulse,
            }
     endpoint = f'/v1/pulses/{pulse_id}.json'
     r = requests.delete(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
-def get_pulse_subscribers(secret, api_url, pulse_id, page_offset, results_per_page, result_offset)
+def get_pulse_subscribers(secret, api_url, pulse_id, page_offset=0, results_per_page=25, result_offset=0)
     data = {
             'api_key': secret,
+            'id': pulse_id,
+            'page': page_offset,
+            'per_page': results_per_page,
+            'offset': result_offset,
            }
     endpoint = f'/v1/pulses/{pulse_id}/subscribers.json'
     r = requests.get(url=api_url+endpoint, data=data)
     return r
 
-def add_pulse_subscriber(secret, api_url, pulse_id, user_id, as_admin):
+def add_pulse_subscriber(secret, api_url, pulse_id, user_id, as_admin=False):
     data = {
             'api_key': secret,
+            'id': pulse_id,
+            'user_id': user_id,
+            'as_admin': as_admin,
            }
     endpoint = f'/v1/pulses/{pulse_id}/subscribers.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def delete_pulse_subscriber(secret, api_url, pulse_id, user_id):
     data = {
             'api_key': secret,
+            'id': pulse_id,
+            'user_id': user_id,
            }
     endpoint = '/v1/pulses/{pulse_id}/subscribers/{user_id}.json'
     r = requests.delete(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def get_notes_from_pulse(secret, api_url, pulse_id):
     data = {
             'api_key': secret,
+            'id': pulse_id,
            }
     endpoint = '/v1/pulses/{pulse_id}/notes.json'
     r = requests.get(url=api_url+endpoint, data=data)
     return r
 
-def create_pulse_note(secret, api_url, pulse_id, note_title, note_content, owners_only, user_id, create_update):
+def create_pulse_note(secret, api_url, pulse_id, note_title, note_content, user_id, owners_only=False, create_update_notification=False):
     data = {
             'api_key': secret,
+            'id': pulse_id,
+            'title': note_title,
+            'content': note_content,
+            'owners_only': owners_only,
+            'user_id': user_id,
+            'create_update': create_update_notification,
            }
     endpoint = '/v1/pulses/{pulse_id}/notes.json'
     r = requests.post(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def delete_pulse_note(secret, api_url, pulse_id, note_id):
     data = {
             'api_key': secret,
+            'id': pulse_id,
+            'note_id': note_id,
            }
     endpoint = '/v1/pulses/{pulse_id}/notes/{note_id}.json'
     r = requests.delete(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
-def get_pulse_updates(secret, api_url, pulse_id, results_per_page, update_limit):
+def get_pulse_updates(secret, api_url, pulse_id, updates_page, results_per_page=25):
     data = {
             'api_key': secret,
+            'id': pulse_id,
+            'page': updates_page,
+            'limit': results_per_page,
            }
     endpoint = '/v1/pulses/{pulse_id}/updates.json'
     r = requests.get(url=api_url+endpoint, data=data)
@@ -219,7 +241,7 @@ def get_pulse_updates(secret, api_url, pulse_id, results_per_page, update_limit)
 
 ### Boards ###
 
-def get_all_boards(secret, api_url, results_per_page, only_globals, order_by_latest):
+def get_all_boards(secret, api_url, results_per_page, only_globals=False, order_by_latest=False):
     data = {
             'api_key': secret,
             'per_page': results_per_page,
@@ -230,7 +252,7 @@ def get_all_boards(secret, api_url, results_per_page, only_globals, order_by_lat
     r = requests.get(url=api_url+endpoint, data=data)
     return r
 
-def create_board(secret, api_url, user_id, board_name, board_description, board_type):
+def create_board(secret, api_url, user_id, board_name, board_description='', board_type=''):
     data = {
             'api_key': secret,
             'user_id': user_id,
@@ -240,7 +262,7 @@ def create_board(secret, api_url, user_id, board_name, board_description, board_
            }
     endpoint = '/v1/boards.json'
     r = requests.post(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def get_board_details(secret, api_url, board_id):
     data = {
@@ -258,9 +280,9 @@ def archive_board(secret, api_url, board_id):
            }
     endpoint = f'/v1/boards/{board_id}.json'
     r = requests.delete(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
-def get_all_board_groups(secret, api_url, board_id, show_archived, show_deleted):
+def get_all_board_groups(secret, api_url, board_id, show_archived=False, show_deleted=False):
     data = {
             'api_key': secret,
             'board_id': board_id,
@@ -281,7 +303,7 @@ def update_group_info(secret, api_url, board_id, group_id, group_title, group_co
            }
     endpoint = f'/v1/boards/{board_id}/groups.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def create_group_in_board(secret, api_url, board_id, group_title):
     data = {
@@ -291,7 +313,7 @@ def create_group_in_board(secret, api_url, board_id, group_title):
            }
     endpoint = f'/v1/boards/{board_id}/groups.json'
     r = requests.post(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def move_group_to board(secret, api_url, board_id, group_id, user_id, destination_board_id):
     data = {
@@ -303,7 +325,7 @@ def move_group_to board(secret, api_url, board_id, group_id, user_id, destinatio
            }
     endpoint = f'/v1/boards/{board_id}/groups/{group_id}/move.json'
     r = requests.post(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def archive_group(secret, api_url, board_id, group_id):
     data = {
@@ -313,9 +335,9 @@ def archive_group(secret, api_url, board_id, group_id):
            }
     endpoint = f'/v1/boards/{board_id}/groups/{group_id}.json'
     r = requests.delete(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
-def get_all_board_columns(secret, api_url, board_id, all_columns):
+def get_all_board_columns(secret, api_url, board_id, all_columns=False):
     data = {
             'api_key': secret,
             'board_id': board_id,
@@ -325,7 +347,7 @@ def get_all_board_columns(secret, api_url, board_id, all_columns):
     r = requests.get(url=api_url+endpoint, data=data)
     return r
 
-def add_new_column_to_board(secret, api_url, board_id, column_name, column_type, column_labels):
+def add_new_column_to_board(secret, api_url, board_id, column_name, column_type, column_labels=''):
     data = {
             'api_key': secret,
             'board_id': board_id,
@@ -335,9 +357,9 @@ def add_new_column_to_board(secret, api_url, board_id, column_name, column_type,
            }
     endpoint = f'/v1/boards/{board_id}/columns.json'
     r = requests.post(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
-def update_column_definition(secret, api_url, board_id, column_id, column_title, column_colour, column_labels):
+def update_column_definition(secret, api_url, board_id, column_id, column_title, column_colour, column_labels=''):
     data = {
             'api_key': secret,
             'board_id': board_id,
@@ -348,7 +370,7 @@ def update_column_definition(secret, api_url, board_id, column_id, column_title,
            }
     endpoint = f'/v1/boards/{board_id}/columns/{column_id}.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def delete_column(secret, api_url, board_id, column_id):
     data = {
@@ -358,9 +380,9 @@ def delete_column(secret, api_url, board_id, column_id):
            }
     endpoint = f'/v1/boards/{board_id}/columns/{column_id}.json'
     r = requests.delete(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
-def get_pulse_column_value(secret, api_url, board_id, column_id, pulse_id, return_as_array):
+def get_pulse_column_value(secret, api_url, board_id, column_id, pulse_id, return_as_array=False):
     data = {
             'api_key': secret,
             'board_id': board_id,
@@ -382,7 +404,7 @@ def update_text_column(secret, api_url, board_id, column_id, pulse_id, text):
            }
     endpoint = f'/v1/boards/{board_id}/columns/{column_id}/text.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def update_person_column(secret, api_url, board_id, column_id, pulse_id, user_id):
     data = {
@@ -394,7 +416,7 @@ def update_person_column(secret, api_url, board_id, column_id, pulse_id, user_id
            }
     endpoint = f'/v1/boards/{board_id}/columns/{column_id}/person.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def update_status_column(secret, api_url, board_id, column_id, pulse_id, colour_index, update_id):
     data = {
@@ -407,7 +429,7 @@ def update_status_column(secret, api_url, board_id, column_id, pulse_id, colour_
            }
     endpoint = f'/v1/boards/{board_id}/columns/{column_id}/status.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def update_date_column(secret, api_url, board_id, column_id, pulse_id, date_string):
     data = {
@@ -419,7 +441,7 @@ def update_date_column(secret, api_url, board_id, column_id, pulse_id, date_stri
            }
     endpoint = f'/v1/boards/{board_id}/columns/{column_id}/date.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def update_numeric_column(secret, api_url, board_id, column_id, pulse_id, numeric_value):
     data = {
@@ -431,7 +453,7 @@ def update_numeric_column(secret, api_url, board_id, column_id, pulse_id, numeri
            }
     endpoint = f'/v1/boards/{board_id}/columns/{column_id}/numeric.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def update_tag_column(secret, api_url, board_id, column_id, pulse_id, tags_string):
     data = {
@@ -443,7 +465,7 @@ def update_tag_column(secret, api_url, board_id, column_id, pulse_id, tags_strin
            }
     endpoint = f'/v1/boards/{board_id}/columns/{column_id}/tags.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def update_timeline_column(secret, api_url, board_id, column_id, pulse_id, from_date, to_date):
     data = {
@@ -456,9 +478,9 @@ def update_timeline_column(secret, api_url, board_id, column_id, pulse_id, from_
            }
     endpoint = f'/v1/boards/{board_id}/columns/{column_id}/timeline.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
-def get_board_pulses(secret, api_url, board_id, page_offset, results_per_page, order_by):
+def get_board_pulses(secret, api_url, board_id, page_offset=0, results_per_page=25, order_by=''):
     data = {
             'api_key': secret,
             'board_id': board_id,
@@ -470,7 +492,7 @@ def get_board_pulses(secret, api_url, board_id, page_offset, results_per_page, o
     r = requests.get(url=api_url+endpoint, data=data)
     return r
 
-def create_pulse(secret, api_url, board_id, user_id, pulse_name, pulse_hash='', pulse_photo_from_url='', pulse_update_hash='', pulse_update_text='', pulse_group_id='', add_to_bottom):
+def create_pulse(secret, api_url, board_id, user_id, pulse_name, pulse_hash='', pulse_photo_from_url='', pulse_update_hash='', pulse_update_text='', pulse_group_id='', add_to_bottom=False):
     data = {
             'api_key': secret,
             'board_id': board_id,
@@ -485,9 +507,9 @@ def create_pulse(secret, api_url, board_id, user_id, pulse_name, pulse_hash='', 
            }
     endpoint = f'/v1/boards/{board_id}/pulses.json'
     r = requests.post(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
-def move_pulse(secret, api_url, board_id, user_id, group_id, pulse_ids, destination_board_id, force_move_to_board):
+def move_pulse(secret, api_url, board_id, user_id, group_id, pulse_ids, destination_board_id, force_move_to_board=False):
     data = {
             'api_key': secret,
             'board_id': board_id,
@@ -499,7 +521,7 @@ def move_pulse(secret, api_url, board_id, user_id, group_id, pulse_ids, destinat
            }
     endpoint = f'/v1/boards/{board_id}/pulses/move.json'
     r = requests.post(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def get_board_subscribers(secret, api_url, board_id, page_offset, results_per_page, result_offset):
     data = {
@@ -513,7 +535,7 @@ def get_board_subscribers(secret, api_url, board_id, page_offset, results_per_pa
     r = requests.get(url=api_url+endpoint, data=data)
     return r
 
-def add_board_subscriber(secret, api_url, board_id, user_id, as_admin):
+def add_board_subscriber(secret, api_url, board_id, user_id, as_admin=False):
     data = {
             'api_key': secret,
             'board_id': board_id,
@@ -522,7 +544,7 @@ def add_board_subscriber(secret, api_url, board_id, user_id, as_admin):
            }
     endpoint = f'/v1/boards/{board_id}/subscribers.json'
     r = requests.put(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 def delete_board_subscriber(secret, api_url, board_id, user_id):
     data = {
@@ -532,7 +554,7 @@ def delete_board_subscriber(secret, api_url, board_id, user_id):
            }
     endpoint = f'/v1/boards/{board_id}/subscribers/{user_id}.json'
     r = requests.delete(url=api_url+endpoint, data=data)
-    return r
+    return r.status_code
 
 ### Tags ###
 
